fea9232817e7a830884abe84f7e2477edc494fd1
philg <philgreening@hotmail.com>
2022-06-07 21:48:42 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,
+         Dimensions, SafeAreaView,
+         TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light content" />
     </View>
   );
 }
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '##000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
