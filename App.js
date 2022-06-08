051faaf999577aedd813326b2cad121251226683
philg <philgreening@hotmail.com>
2022-06-08 23:40:28 +0100

    added buttons and pressed fucntion

--- App.js
+++ App.js
@@ -4,20 +4,175 @@ import { StyleSheet, Text, View,
          TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 
+const screen = Dimensions.get("window");
+const btnWidth = screen.width / 4;
+const width = screen.width;
+
 export default function App() {
+
+  const [answerValue, setAnswerValue] = useState(0);
+
+  function buttonPressed(value) {
+    alert('You tapped button ' + value);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="light content" />
-    </View>
+    <SafeAreaView style={[styles.container, styles.safeArea]}>
+      <View style={styles.container}>
+        <Text style={styles.number}>{ answerValue }</Text>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('C')}>
+              <Text style={styles.buttonTextDark}>C</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('+/-')}>
+              <Text style={styles.buttonTextDark}>+/-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('%')}>
+              <Text style={styles.buttonTextDark}>%</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('/')}>
+              <Text style={styles.buttonText}>/</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(7)}>
+              <Text style={styles.buttonText}>7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(8)}>
+              <Text style={styles.buttonText}>8</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(9)}>
+              <Text style={styles.buttonText}>9</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('x')}>
+              <Text style={styles.buttonText}>×</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(4)}>
+              <Text style={styles.buttonText}>4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(5)}>
+              <Text style={styles.buttonText}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(6)}>
+              <Text style={styles.buttonText}>6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('-')}>
+              <Text style={styles.buttonText}>−</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(1)}>
+              <Text style={styles.buttonText}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(2)}>
+              <Text style={styles.buttonText}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(3)}>
+              <Text style={styles.buttonText}>3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('+')}>
+              <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.darkButtonLong} onPress={() => buttonPressed(0)}>
+              <Text style={styles.buttonLongText}>0</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed('.')}>
+              <Text style={styles.buttonText}>.</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('=')}>
+              <Text style={styles.buttonText}>=</Text>
+            </TouchableOpacity>
+          </View>
+
+
+
+
+        <StatusBar style='light-content'/>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '##000000',
+    backgroundColor: '#000000',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    padding: '3%',
+  },
+  number: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: width * 0.2,
+    marginRight: '3%',
+  },
+  safeArea: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  greyButton: {
+    flex: 1,
+    backgroundColor: '#a6a6a6',
+    height: btnWidth * 0.8,
+    borderRadius: btnWidth,
+    margin: '1%',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  blueButton:{
+    flex: 1,
+    backgroundColor: '#0984e3',
+    height: btnWidth * 0.8,
+    borderRadius: btnWidth,
+    margin: '1%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  darkButton: {
+    flex: 1,
+    backgroundColor: '#333333',
+    height: btnWidth * 0.8,
+    borderRadius: btnWidth,
+    margin: '1%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  darkButtonLong: {
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#333333',
+    height: btnWidth * 0.8,
+    borderRadius: btnWidth,
+    margin: '1%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  buttonText: {
+    fontSize: width * 0.06,
+    color: 'white',
+  },
+  buttonTextDark: {
+    fontSize: width * 0.06,
+    color: 'black',
+  },
+  buttonLongText:{
+    fontSize: width * 0.06,
+    color: 'white',
+    left: '40%'
+  }
 });
