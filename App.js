cc10f644ca1f45b32d9c0d97603580750ef24fb0
philg <philgreening@hotmail.com>
2022-06-10 00:31:57 +0100

    started to add functionality

--- App.js
+++ App.js
@@ -11,94 +11,130 @@ const width = screen.width;
 export default function App() {
 
   const [answerValue, setAnswerValue] = useState(0);
+  const [readyToReplace, setReadyToReplace] = useState(true)
+
+  const [memoryValue, setMemoryValue] = useState(0);
+  const [operatorValue, setOperatorValue] = useState(0);
 
   function buttonPressed(value) {
-    alert('You tapped button ' + value);
+
+    if (Number.isInteger(value)){
+      handleNumber(value);
+    }
+    else if(value === 'C') { // resets value to zero
+      setAnswerValue(0);
+      setMemoryValue(0);
+      setOperatorValue(0);
+      setReadyToReplace(true);
+    }
+    else if(value === '/' || value === 'x' || value === '-' || value === '+') {
+      setMemoryValue(answerValue);
+      setReadyToReplace(true);
+      setOperatorValue(value);
+      console.log(operatorValue, memoryValue);
+    }
+    else if(value === '=') {
+
+    }
+
+  }
+
+  function handleNumber(value) {
+    // alert('Called ' + value);
+    if(readyToReplace) {
+      alert('rtr ' + value );
+      setAnswerValue(value);
+    }else{
+      alert('called');
+      setAnswerValue(answerValue.toString() + value.toString());
+    }
+    return answerValue;
+  }
+
+  function calculatesEquals() {
+    
   }
 
   return (
-    <SafeAreaView style={[styles.container, styles.safeArea]}>
-      <View style={styles.container}>
+    <View style={[styles.container]}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.number}>{ answerValue }</Text>
 
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('C')}>
-              <Text style={styles.buttonTextDark}>C</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('+/-')}>
-              <Text style={styles.buttonTextDark}>+/-</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('%')}>
-              <Text style={styles.buttonTextDark}>%</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('/')}>
-              <Text style={styles.buttonText}>/</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('C')}>
+            <Text style={styles.buttonTextDark}>C</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('+/-')}>
+            <Text style={styles.buttonTextDark}>+/-</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.greyButton} onPress={() => buttonPressed('%')}>
+            <Text style={styles.buttonTextDark}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('/')}>
+            <Text style={styles.buttonText}>/</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(7)}>
-              <Text style={styles.buttonText}>7</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(8)}>
-              <Text style={styles.buttonText}>8</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(9)}>
-              <Text style={styles.buttonText}>9</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('x')}>
-              <Text style={styles.buttonText}>×</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(7)}>
+            <Text style={styles.buttonText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(8)}>
+            <Text style={styles.buttonText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(9)}>
+            <Text style={styles.buttonText}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('x')}>
+            <Text style={styles.buttonText}>×</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(4)}>
-              <Text style={styles.buttonText}>4</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(5)}>
-              <Text style={styles.buttonText}>5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(6)}>
-              <Text style={styles.buttonText}>6</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('-')}>
-              <Text style={styles.buttonText}>−</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(4)}>
+            <Text style={styles.buttonText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(5)}>
+            <Text style={styles.buttonText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(6)}>
+            <Text style={styles.buttonText}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('-')}>
+            <Text style={styles.buttonText}>−</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(1)}>
-              <Text style={styles.buttonText}>1</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(2)}>
-              <Text style={styles.buttonText}>2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(3)}>
-              <Text style={styles.buttonText}>3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('+')}>
-              <Text style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(1)}>
+            <Text style={styles.buttonText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(2)}>
+            <Text style={styles.buttonText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed(3)}>
+            <Text style={styles.buttonText}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('+')}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+        </View>
 
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.darkButtonLong} onPress={() => buttonPressed(0)}>
-              <Text style={styles.buttonLongText}>0</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed('.')}>
-              <Text style={styles.buttonText}>.</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('=')}>
-              <Text style={styles.buttonText}>=</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.darkButtonLong} onPress={() => buttonPressed(0)}>
+            <Text style={styles.buttonLongText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.darkButton} onPress={() => buttonPressed('.')}>
+            <Text style={styles.buttonText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.blueButton} onPress={() => buttonPressed('=')}>
+            <Text style={styles.buttonText}>=</Text>
+          </TouchableOpacity>
+        </View>
 
-
-
-
-        <StatusBar style='light-content'/>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+      <StatusBar style="light"/>
+    </View>
   );
 }
 
@@ -115,11 +151,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: width * 0.2,
     marginRight: '3%',
-  },
-  safeArea: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
   },
   row: {
     flexDirection: 'row',
